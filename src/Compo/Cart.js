636f43a185afecf87171817749a29f6e5665d11a
Chirag Mangukiya <cdmi.chirag@gmail.com>
2023-12-27 19:37:11 +0530

    cart

--- src/Compo/Cart.js
+++ src/Compo/Cart.js
@@ -1,22 +1,45 @@
 import React from 'react'
+import { Container, Row, Col, Card } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 function Cart() {
 
      const cartItem = useSelector((state)=>state.cart.cartItem)
 
   return (
-    <div>
+    <>
+    <Container>
+          <Row>
           {
                cartItem.map((item)=>{
                     return(
                          <>
-                              
+                              <Col lg={4} md={6} xs={12}>
+                                   <Card>
+                                        <div className='pro_img'>
+                                             <Card.Img variant="top" src={item.thumbnail} />
+                                             <div className="discount">
+                                                  {Math.round(item.discountPercentage)}%<br></br> OFF
+                                             </div>
+                                        </div>
+                                        <Card.Body>
+                                             <Card.Text>
+                                                  <div class="pro_details">
+                                                       <Link to={`/product/${item.id}`} class="pro_name text-truncate">{item.title}</Link>
+                                                       <p class="pro_price">${item.price}</p>
+                                                  </div>
+                                             </Card.Text>
+                                        </Card.Body>
+                                   </Card>
+                              </Col>
                          </>
                     )
                })
           }
-    </div>
+          </Row>
+     </Container>
+    </>
   )
 }
 
